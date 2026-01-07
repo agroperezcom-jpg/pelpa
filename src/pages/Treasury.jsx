0b219d944847f59d0bcf171c39d1e047352427c7
Gerardo Perez <agroperezcom@gmail.com>
2026-01-07 18:02:26 +0000

    File changes

--- src/pages/Treasury.jsx
+++ src/pages/Treasury.jsx
@@ -11,10 +11,10 @@ import {
   Plus,
   DollarSign
 } from "lucide-react";
-import CuentasView from "../components/treasury/CuentasView";
-import MovimientosView from "../components/treasury/MovimientosView";
-import ChequesView from "../components/treasury/ChequesView";
-import CobrosView from "../components/treasury/CobrosView";
+import CuentasView from "../components/treasury/CuentasView.js";
+import MovimientosView from "../components/treasury/MovimientosView.js";
+import ChequesView from "../components/treasury/ChequesView.js";
+import CobrosView from "../components/treasury/CobrosView.js";
 
 export default function Treasury() {
   const { data: accounts = [] } = useQuery({
