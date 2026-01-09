3f1ec593d66fc6ee826c53ec27b56a24fa00e558
Gerardo Perez <agroperezcom@gmail.com>
2026-01-09 21:36:06 +0000

    File changes

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -556,6 +556,7 @@ export default function Calendar() {
           currentDate={currentDate}
           events={events}
           onEventClick={handleEventClick}
+          onEventDrop={handleEventDrop}
         />
       )}
 
