8d2ea6b49bd2bf931c2d2547a6394a9fcff0fff8
Gerardo Perez <agroperezcom@gmail.com>
2026-01-09 18:50:56 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -286,7 +286,7 @@ export default function Layout({ children, currentPageName }) {
         "fixed top-0 left-0 bottom-0 w-64 bg-card border-r border-border/40 z-50 transition-theme",
         "transition-transform duration-300 ease-out",
         "lg:translate-x-0",
-        sidebarOpen ? "translate-x-0" : "-translate-x-full"
+        sidebarOpen || sidebarPinned ? "translate-x-0" : "-translate-x-full"
       )}>
         <div className="flex flex-col h-full">
           {/* Logo */}
