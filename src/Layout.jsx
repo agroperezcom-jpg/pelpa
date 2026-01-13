201f6f2bfea98d270fb19e41f8c342ae390fbe22
Gerardo Perez <agroperezcom@gmail.com>
2026-01-13 23:04:03 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,4 +1,3 @@
-
 import React, { useState, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { createPageUrl } from "./utils";
@@ -590,9 +589,9 @@ export default function Layout({ children, currentPageName }) {
           </div>
         </DialogContent>
       </Dialog>
-      </div>
-      </ThemeProvider>
+          </div>
+        </ThemeProvider>
       </CompanyProvider>
-      </ExternalAuthProvider>
-      );
-      }
+    </ExternalAuthProvider>
+  );
+}
