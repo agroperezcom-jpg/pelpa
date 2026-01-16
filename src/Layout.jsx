05b6b3bd76b82cd11bd994a8b3f454475bb4925e
Gerardo Perez <agroperezcom@gmail.com>
2026-01-16 20:18:08 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -29,6 +29,6 @@ export default function Layout({ children, currentPageName }) {
                     {children}
                   </LayoutContent>
                 </ThemeProvider>
-              </ExternalAuthProvider>
-          );
-          }
+                </ExternalAuthProvider>
+                );
+                }
